33671da065f2998a8995c6981b818e2b5b64b9e6
Konstantinos Nikopoulos <knikopoulos3@gmail.com>
2022-10-19 10:31:18 +0300

    stage comments del

--- main.js
+++ main.js
@@ -23,8 +23,7 @@ var composer;
 var skydom;
 var skydomWithTexture;
 var skydomTexture;
-//before stage
-//after stage
+
 const skyboxPath = 'skybox/clouds1/' ;
 const front = "clouds1_north.bmp";
 const back = "clouds1_south.bmp";
